5069be3317675c7942707ab1560c3e3252f9c150
John Heroy <johnheroy@gmail.com>
2014-11-20 10:46:42 -0800

    Basic layout with static data

--- src/app.js
+++ src/app.js
@@ -3,9 +3,38 @@
 
   var myApp = angular.module('myApp', ['tc.chartjs']);
 
-  myApp.controller('AppController', ['$scope', function($scope){
+  myApp.controller('ChartsController', ['$scope', function($scope){
 
-    $scope.someNumber = 123;
+    // line chart
+    $scope.activityData = {
+      labels: ["January", "February", "March", "April", "May", "June", "July"],
+      datasets: [
+        {
+          label: "My First dataset",
+          fillColor: "rgba(220,220,220,0.2)",
+          strokeColor: "rgba(220,220,220,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+          label: "My Second dataset",
+          fillColor: "rgba(151,187,205,0.2)",
+          strokeColor: "rgba(151,187,205,1)",
+          pointColor: "rgba(151,187,205,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(151,187,205,1)",
+          data: [28, 48, 40, 19, 86, 27, 90]
+        }
+      ]
+    };
+
+    $scope.activityOptions = {
+
+    };
 
     $scope.myData = [
       { value : 50, color : "#F7464A" },
